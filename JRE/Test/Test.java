15242c9532ee59218ea882ecfa910546bfd11561
sjaisankar <sjaisankar@IN9X3X3W2.CSCMWS.CSCMWS.COM>
2020-03-11 19:26:46 +0530

    Sec commit...

---
+++ JRE/Test/Test.java
@@ -0,0 +1,15 @@
+package Test;
+
+public class Test {
+	public static void main(String[] args) {
+		String ticket = "yes" ;
+		if (ticket == "yes")
+		System.out.println("get ticket...");
+		else {
+			System.out.println("Don't go...");
+		}
+			
+				
+	}
+
+}
